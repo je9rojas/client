69e739fe98f04e0037e1a6452820bace7d78c4cd
je9rojas <je9rojas@gmail.com>
2024-11-07 16:09:22 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
 
+//25:12
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,10 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
